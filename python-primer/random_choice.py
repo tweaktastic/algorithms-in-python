297f89c38e5fab22650463e4203bed42aed71e20
tweaktastic <abhinav.sharma@nuvoex.com>
2017-05-15 13:10:52 +0530

    End of Excercise 1

---
+++ python-primer/random_choice.py
@@ -0,0 +1,11 @@
+import random
+
+def random_choice(seq):
+    try:
+        return seq[random.randrange(len(seq))]
+    except IndexError:
+        raise()
+
+
+if __name__ == '__main__':
+    print(random_choice([4,5,6,7,8,10,23,44,55,43,563,23,124,24,32,4543]))
